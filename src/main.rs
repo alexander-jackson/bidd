d975cbfcc8a860b7882c3698b99ce5638d0da7d1
Alexander Jackson <alex.jackson@starlingbank.com>
2024-06-20 20:28:31 +0100

    feat: use `blueutil` instead

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use btleplug::api::{Central, CentralEvent, Manager as _, Peripheral, ScanFilter};
-use btleplug::platform::Manager;
-use color_eyre::eyre::{eyre, Result};
-use futures_util::StreamExt;
+use std::process::Command;
+
+use color_eyre::eyre::Result;
 
 const HEADPHONE_NAME: &'static str = "WH-1000XM4";
-const DEVICE_IDENTIFIERS: &[&'static str] = &[
-    // "ff3dd8eb-e4b9-b66f-7d04-0e57e7a30766",
-    "22a6763b-71bb-27fb-61f0-ac0fa7fd7ab1",
-];
 
 fn setup() -> Result<()> {
     color_eyre::install()?;
@@ -16,63 +11,21 @@ fn setup() -> Result<()> {
     Ok(())
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     setup()?;
 
-    let headphones_on = are_headphones_on(HEADPHONE_NAME).await?;
-    tracing::info!(%headphones_on, "Checked the system state");
+    if !are_headphones_on(HEADPHONE_NAME)? {
+        Command::new("blueutil").args(["--power", "0"]).spawn()?;
+
+        tracing::info!("Headphones are not on, disabled Bluetooth");
+    }
 
     Ok(())
 }
 
-async fn are_headphones_on(name: &str) -> Result<bool> {
-    let manager = Manager::new().await?;
-    let adapters = manager.adapters().await?;
+fn are_headphones_on(name: &str) -> Result<bool> {
+    let output = Command::new("blueutil").args(["--connected"]).output()?;
+    let stdout = std::str::from_utf8(&output.stdout)?;
 
-    let central_adapter = adapters
-        .first()
-        .ok_or_else(|| eyre!("Failed to find any adapters"))?;
-
-    central_adapter.start_scan(ScanFilter::default()).await?;
-
-    // Subscribe to events
-    let mut events = central_adapter.events().await?;
-
-    while let Some(event) = events.next().await {
-        let id = match &event {
-            CentralEvent::DeviceDiscovered(id)
-            | CentralEvent::DeviceUpdated(id)
-            | CentralEvent::DeviceConnected(id)
-            | CentralEvent::DeviceDisconnected(id)
-            | CentralEvent::ManufacturerDataAdvertisement { id, .. }
-            | CentralEvent::ServiceDataAdvertisement { id, .. }
-            | CentralEvent::ServicesAdvertisement { id, .. } => id,
-        };
-
-        if DEVICE_IDENTIFIERS.contains(&id.to_string().as_str()) {
-            tracing::info!(?event, "Got an event for a known device");
-        }
-
-        match event {
-            CentralEvent::DeviceDiscovered(id) => {
-                let peripheral = central_adapter.peripheral(&id).await?;
-
-                if peripheral
-                    .properties()
-                    .await?
-                    .and_then(|properties| properties.local_name)
-                    .is_some_and(|value| value.contains(name))
-                {
-                    tracing::info!(%id, properties = ?peripheral.properties().await?, "Headphones have been discovered");
-                }
-            }
-            CentralEvent::DeviceDisconnected(id) => {
-                tracing::info!(%id, "Device has been disconnected");
-            }
-            _ => (),
-        }
-    }
-
-    Ok(false)
+    Ok(stdout.lines().any(|line| line.contains(name)))
 }
