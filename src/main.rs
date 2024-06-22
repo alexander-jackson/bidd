f29516fbe3a09f33d4cc261b546bcf86d5446efb
Alexander Jackson <alex.jackson@starlingbank.com>
2024-06-22 11:13:59 +0100

    feat: add installation command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::fs::OpenOptions;
 use std::time::Duration;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
+use tera::{Context, Tera};
 
 mod bluetooth;
 mod blueutil;
@@ -43,9 +45,41 @@ where
     }
 }
 
-fn main() -> Result<()> {
-    setup()?;
+fn install_launchd_configuration() -> Result<()> {
+    let template = include_str!("../resources/bidd.plist");
+    let mut tera = Tera::default();
 
+    tera.add_raw_template("plist", &template)?;
+
+    let home_dir = dirs::home_dir().ok_or_else(|| eyre!("Failed to get home directory"))?;
+    let binary_path = home_dir.join(".cargo").join("bin").join("bidd");
+
+    let config_path = home_dir
+        .join("Library")
+        .join("LaunchAgents")
+        .join("bidd.plist");
+
+    let config_file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(&config_path)?;
+
+    let mut context = Context::new();
+    context.insert("binary_path", &binary_path);
+
+    tera.render_to("plist", &context, config_file)?;
+
+    tracing::info!(
+        ?binary_path,
+        ?config_path,
+        "Installed `launchd` configuration"
+    );
+
+    Ok(())
+}
+
+fn poll_for_changes() -> Result<()> {
     let handler: LifecycleHandler<BlueutilController> = LifecycleHandler::default();
 
     loop {
@@ -54,6 +88,17 @@ fn main() -> Result<()> {
         // Wait for a while to check again
         std::thread::sleep(Duration::from_secs(60));
     }
+}
+
+fn main() -> Result<()> {
+    setup()?;
+
+    match std::env::args().skip(1).next() {
+        Some(value) if value == "setup" => install_launchd_configuration()?,
+        _ => poll_for_changes()?,
+    };
+
+    Ok(())
 }
 
 #[cfg(test)]
